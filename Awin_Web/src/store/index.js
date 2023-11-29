4349d863e417d5836678d662ca554607aa956ad7
RainBowT <rainbowt0506@gmail.com>
2023-11-29 21:48:09 +0800

    adjust: Redirect to dashboard upon successful login

--- Awin_Web/src/store/index.js
+++ Awin_Web/src/store/index.js
@@ -1,101 +1,97 @@
-import { createStore } from "vuex";
-import router from "../router";
-import { auth } from "../firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
+import { createStore } from 'vuex'
+import router from '../router'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 
 export default createStore({
   state: {
-    user: null,
+    user: null
   },
   mutations: {
     SET_USER(state, user) {
-      state.user = user;
+      state.user = user
     },
     CLEAR_USER(state) {
-      state.user = null;
-    },
+      state.user = null
+    }
   },
   actions: {
     async login({ commit }, details) {
-      const { email, password } = details;
+      const { email, password } = details
 
       try {
-        await signInWithEmailAndPassword(auth, email, password);
+        await signInWithEmailAndPassword(auth, email, password)
       } catch (error) {
         switch (error.code) {
-          case "auth/user-not-found":
-            alert("User not found");
-            break;
-          case "auth/wrong-password":
-            alert("Wrong password");
-            break;
+          case 'auth/user-not-found':
+            alert('User not found')
+            break
+          case 'auth/wrong-password':
+            alert('Wrong password')
+            break
           default:
-            alert("Something went wrong");
+            alert('Something went wrong')
         }
 
-        return;
+        return
       }
 
-      commit("SET_USER", auth.currentUser);
+      commit('SET_USER', auth.currentUser)
 
-      router.push("/");
+      router.push('/dashboard')
     },
 
     async register({ commit }, details) {
-      const { email, password } = details;
+      const { email, password } = details
 
       try {
-        await createUserWithEmailAndPassword(auth, email, password);
+        await createUserWithEmailAndPassword(auth, email, password)
       } catch (error) {
         switch (error.code) {
-          case "auth/email-already-in-use":
-            alert("Email already in use");
-            break;
-          case "auth/invalid-email":
-            alert("Invalid email");
-            break;
-          case "auth/operation-not-allowed":
-            alert("Operation not allowed");
-            break;
-          case "auth/weak-password":
-            alert("Weak password");
-            break;
+          case 'auth/email-already-in-use':
+            alert('Email already in use')
+            break
+          case 'auth/invalid-email':
+            alert('Invalid email')
+            break
+          case 'auth/operation-not-allowed':
+            alert('Operation not allowed')
+            break
+          case 'auth/weak-password':
+            alert('Weak password')
+            break
           default:
-            alert("Something went wrong");
+            alert('Something went wrong')
         }
 
-        return;
+        return
       }
 
-      commit("SET_USER", auth.currentUser);
+      commit('SET_USER', auth.currentUser)
 
-      router.push("/");
+      router.push('/')
     },
 
     async logout({ commit }) {
-      await signOut(auth);
+      await signOut(auth)
 
-      commit("CLEAR_USER");
+      commit('CLEAR_USER')
 
-      router.push("/login");
+      router.push('/login')
     },
 
     fetchUser({ commit }) {
       auth.onAuthStateChanged(async (user) => {
         if (user === null) {
-          commit("CLEAR_USER");
+          commit('CLEAR_USER')
         } else {
-          commit("SET_USER", user);
+          commit('SET_USER', user)
 
-          if (router.isReady() && router.currentRoute.value.path === "/login") {
-            router.push("/");
+          if (router.isReady() && router.currentRoute.value.path === '/login') {
+            router.push('/')
           }
         }
-      });
-    },
-  },
-});
+      })
+    }
+  }
+})
